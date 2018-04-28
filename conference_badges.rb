e1ea20ac2c748fe8e45bfd196e8eaaab9f12678c
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 00:22:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,15 +3,13 @@
 def assign_rooms(name)
   room_number = @room_list.shift()
   puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
-  room_number
 end
 
 def badge_maker(name)
-  badge = "Hello, my name is #{name}" 
+  badge = "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(names)
-  names.each {|name| }
 end
 
 def printer()
