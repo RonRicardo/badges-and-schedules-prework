35a23c02443e9aa54b05d765e9e4507d8ef5f446
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 00:28:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,13 @@
 @room_list = Array(1..7)
 
-def assign_rooms(name)
+def assign_rooms(names)
   room_number = 1
   room_assignments = []
-  room_message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
-  room_assignments << room_message
-  room_assignments
+  names.each |name| do
+    room_message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+    room_assignments << room_message
+  end
+    room_assignments
 end
 
 def badge_maker(name)
