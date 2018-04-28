fccf252c81a5be65445443ca7a7da8280cc88e6c
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 00:07:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,11 @@
 
 def assign_rooms(name)
   room_number = @room_list.shift()
-  assign_message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  puts "Hello, #{name}! You'll be assigned to room #{room_number}!"
+  room_number
 end
 
 def batch_badge_creator(names)
-  names.each{ |names| names.assign_rooms}
+  names.each {|name| name.assign_rooms}
   #out: array of badge messages
 end
