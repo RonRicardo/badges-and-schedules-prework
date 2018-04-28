d6a4e6c59821bdef10b21ecef66ffd8f33bbca3e
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-28 00:01:13 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
+@room_list = Array(1..7)
 
 def assign_rooms(name)
-  room_list = Array(1..7)
-  room_number = room_list.shift()
+  room_number = @room_list.shift()
   assign_message = "Hello, #{name}! You'll be assigned to room #{room_number}!"
 end
 
